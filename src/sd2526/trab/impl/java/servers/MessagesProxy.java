b176d460cba07b3036253fa65360149177855060
Artur <a.supelnic@campus.fct.unl.pt>
2026-05-12 10:10:54 +0100

    MessageProxy done (no testing yet)

--- src/sd2526/trab/impl/java/servers/MessagesProxy.java
+++ src/sd2526/trab/impl/java/servers/MessagesProxy.java
@@ -5,8 +5,10 @@ import static sd2526.trab.api.java.Result.ok;
 import static sd2526.trab.api.java.Result.ErrorCode.BAD_REQUEST;
 import static sd2526.trab.api.java.Result.ErrorCode.FORBIDDEN;
 import static sd2526.trab.api.java.Result.ErrorCode.INTERNAL_ERROR;
+import static sd2526.trab.api.java.Result.ErrorCode.NOT_FOUND;
 
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import sd2526.trab.api.Message;
@@ -15,12 +17,13 @@ import sd2526.trab.api.java.Messages;
 import sd2526.trab.api.java.Result;
 import sd2526.trab.api.java.Result.ErrorCode;
 import sd2526.trab.impl.java.clients.Clients;
+import sd2526.trab.impl.utils.IP;
 import sd2526.trab.impl.zoho.Zoho;
 import sd2526.trab.impl.zoho.msgs.ZohoMessage;
 
 public class MessagesProxy implements Messages {
 
-  private static Logger Log = Logger.getLogger(JavaMessages.class.getName());
+  private static Logger Log = Logger.getLogger(MessagesProxy.class.getName());
 
   private final Zoho zoho = Zoho.getInstance();
 
@@ -62,32 +65,135 @@ public class MessagesProxy implements Messages {
 
   @Override
   public Result<Message> getInboxMessage(String name, String mid, String pwd) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getInboxMessage'");
+    Log.info(() -> "getInboxMessage : name = %s, mid = %s, pwd = %s\n".formatted(name, mid, pwd));
+    if (name == null || mid == null || pwd == null) {
+      return Result.error(BAD_REQUEST);
+    }
+
+    try {
+      var userResult = getUser(name, pwd);
+      if (!userResult.isOK())
+        return Result.error(userResult.error());
+
+      ZohoMessage zohoMsg = zoho.getMessage(mid);
+      if (zohoMsg == null) {
+        return Result.error(NOT_FOUND);
+      } else {
+        Message msg = new Message(
+            zohoMsg.messageId(),
+            zohoMsg.fromAddress(),
+            Set.of(zohoMsg.toAddress().split(",")),
+            zohoMsg.subject(),
+            zohoMsg.content());
+        return Result.ok(msg);
+      }
+    } catch (Exception x) {
+      x.printStackTrace();
+      return Result.error(INTERNAL_ERROR);
+    }
   }
 
   @Override
   public Result<List<String>> getAllInboxMessages(String name, String pwd) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getAllInboxMessages'");
+    Log.info(() -> "getAllInboxMessages : name = %s, pwd = %s\n".formatted(name, pwd));
+
+    if (name == null || pwd == null) {
+      return Result.error(BAD_REQUEST);
+    }
+
+    try {
+      var userResult = getUser(name, pwd);
+      if (!userResult.isOK())
+        return Result.error(userResult.error());
+      var inboxMessages = zoho.getMessages(null);
+      return Result.ok(inboxMessages);
+    } catch (Exception x) {
+      x.printStackTrace();
+      return Result.error(INTERNAL_ERROR);
+    }
+
   }
 
   @Override
   public Result<Void> removeInboxMessage(String name, String mid, String pwd) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'removeInboxMessage'");
+    Log.info(() -> "removeInboxMessage : name = %s, mid = %s, pwd = %s\n".formatted(name, mid, pwd));
+    if (name == null || mid == null || pwd == null) {
+      return Result.error(BAD_REQUEST);
+    }
+    try {
+      var userResult = getUser(name, pwd);
+      if (!userResult.isOK())
+        return Result.error(userResult.error());
+
+      ZohoMessage zohoMsg = zoho.getMessage(mid);
+      if (zohoMsg == null) {
+        return Result.error(NOT_FOUND);
+      }
+      zoho.removeInboxMessage(mid);
+      return Result.ok(null);
+    } catch (Exception x) {
+      x.printStackTrace();
+      return Result.error(INTERNAL_ERROR);
+    }
   }
 
   @Override
   public Result<Void> deleteMessage(String name, String mid, String pwd) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'deleteMessage'");
+    Log.info(() -> "deleteMessage : name = %s, mid = %s, pwd = %s\n".formatted(name, mid, pwd));
+    if (name == null || mid == null || pwd == null) {
+      return Result.error(BAD_REQUEST);
+    }
+    try {
+      var userResult = getUser(name, pwd);
+      if (!userResult.isOK())
+        return Result.error(userResult.error());
+      ZohoMessage zohoMsg = zoho.getMessage(mid);
+      if (zohoMsg == null) {
+        return Result.ok(null);
+      }
+      if (!zohoMsg.fromAddress().equals(name + "@" + userResult.value().getDomain())) {
+        return Result.error(FORBIDDEN);
+      }
+      if (zohoMsg.receivedTime() == null
+          || System.currentTimeMillis() - Long.parseLong(zohoMsg.receivedTime()) > 30_000) {
+        return Result.ok(null);
+      }
+      for (String dest : zohoMsg.toAddress().split(",")) {
+        dest = dest.trim();
+        var domain = dest.split("@", 2)[1];
+
+        if (domain.equals(IP.domain())) {
+          zoho.removeInboxMessage(mid);
+        } else {
+          Clients.AdminMessagesClient.get(domain).remoteDeleteMessage(mid); // not sure if works
+        }
+      }
+      return Result.ok(null);
+
+    } catch (Exception x) {
+      x.printStackTrace();
+      return Result.error(INTERNAL_ERROR);
+    }
   }
 
   @Override
   public Result<List<String>> searchInbox(String name, String pwd, String query) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'searchInbox'");
+    Log.info(() -> "searchInbox : name = %s, pwd = %s, query=%s\n".formatted(name, pwd, query));
+
+    if (name == null || pwd == null || query == null) {
+      return Result.error(BAD_REQUEST);
+    }
+
+    try {
+      var userResult = getUser(name, pwd);
+      if (!userResult.isOK())
+        return Result.error(userResult.error());
+      var inboxMessages = zoho.getMessages(query);
+      return Result.ok(inboxMessages);
+    } catch (Exception x) {
+      x.printStackTrace();
+      return Result.error(INTERNAL_ERROR);
+    }
   }
 
   protected Result<User> getUser(String user, String pwd) {
